e3d5774cdcb10389a2986e911535705d19478318
jurkun <trieyono@gmail.com>
2026-08-19 11:19:54 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "doa-wirid-cache-v3";
+const CACHE_NAME = "doa-wirid-cache-v4";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
+  "./icon-512-maskable.png",
   "./screenshot-1.png",
   "./screenshot-2.png"
 ];
